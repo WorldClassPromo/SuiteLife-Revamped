4c7c1fc8fd5d6a19657e26c291abb31166c98be7
BenjaminWCP <benjamin@worldclasspromo.ca>
2015-04-07 11:19:10 -0400

    Creating a Test Theme

---
+++ wp-content/themes/SuiteLife(Macaron)/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+    	<title>Suite Life</title>
+        <link rel='stylesheet' href='<?php bloginfo('stylesheet_url'); ?>'>
+    </head>
+    <body>
+    	<div id='wrapper'>
+        <div id='header'>
+        <h1>A HEADER</h1>
+        </div>
