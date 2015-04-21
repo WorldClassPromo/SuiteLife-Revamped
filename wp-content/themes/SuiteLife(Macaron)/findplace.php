d53f2293d146413b300b95e6e05d0ceac09adadd
BenjaminWCP <benjamin@worldclasspromo.ca>
2015-04-21 10:17:39 -0400

    From SuiteLife Revamped to SuiteLife-Revamped
    The hypen in between made all the difference.

--- wp-content/themes/SuiteLife(Macaron)/findplace.php
+++ wp-content/themes/SuiteLife(Macaron)/findplace.php
@@ -1,3 +1,17 @@
 <?php
-
+	#####
+	#####
+	// But now Benjamin decided to add this section (o_O) Don't make that face, you'll get it at some point (^_^)
+	#####
+	#####
+	//Benjamin created this file on his computer
+	//It went to the server, and now I can see it can
+	//add stuff to it
+	//By Alice
+	//Are you follwing me?
+	/*
+		We have the file findplace.php I created inside the theme Macaron
+		But you can see it as well. In fact, let me just save this file on your 
+		computer and see what happens
+	*/
 ?>
--- wp-content/themes/SuiteLife(Macaron)/footer.php
+++ wp-content/themes/SuiteLife(Macaron)/footer.php
@@ -1,3 +1,6 @@
-<?php
-
-?>
+            <div id='footer'>
+                <h1>FOOTER</h1>
+            </div>
+        </div>
+    </body>
+</html>
